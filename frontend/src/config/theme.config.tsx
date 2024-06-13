0495bf1b1218a7054a92b4ff1f9844e55ebc342f
alvareztomas1 <47418131+alvareztomas1@users.noreply.github.com>
2024-06-12 21:47:54 -0300

    Adds a Mui ThemeProvider
    Creates a ThemeProvider with an initial configuration, establishing default background, main colors, and typographyy to wrap the application and provide the custom theme.
    
    Utilizes the CssBaseline component to ensure the application-wide usage of these styles.

---
+++ frontend/src/config/theme.config.tsx
@@ -0,0 +1,37 @@
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import React from 'react';
+
+type ThemeProp = {
+  children: JSX.Element;
+};
+
+export enum themePalette {
+  BG = '#0F1C2E',
+  BG_2 = '#1f2b3e',
+  PRIMARY = '#acc2ef',
+  FONT_GLOBAL = 'Roboto',
+}
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    background: {
+      default: themePalette.BG,
+    },
+    primary: {
+      main: themePalette.PRIMARY,
+    },
+  },
+  typography: {
+    fontFamily: themePalette.FONT_GLOBAL,
+  },
+});
+
+export const ThemeConfig: React.FC<ThemeProp> = ({ children }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
+};
