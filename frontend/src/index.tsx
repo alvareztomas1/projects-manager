500a2360031d18849541f3118646916cacb8ccc6
alvareztomas1 <47418131+alvareztomas1@users.noreply.github.com>
2024-06-19 20:53:32 -0300

    Front: Updates App component with the Redux Provider
    Wrap the App component with the Redux Provider to provide access to Redux state across the app.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeConfig } from './config/theme.config';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -11,7 +13,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ThemeConfig>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeConfig>
   </React.StrictMode>,
 );
