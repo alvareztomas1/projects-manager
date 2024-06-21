5b522239dfcc0948d9b1a29d0b2dc55e70cd3315
alvareztomas1 <47418131+alvareztomas1@users.noreply.github.com>
2024-06-21 12:22:45 -0300

    Front: Adds session persistence with access token cookie
    - In RouterLayout.tsx:
    Adds effects to set cookies when the accessToken value from the auth reducer changes (i.e., when the user logs in) and another effect to remove the cookie if it has expired.
    
    Changes the way the component handles error notifications; it now uses an effect.
    
    - In NavBar.tsx:
    Replaces the 'login' and 'register' buttons with a single 'logout' button. The navbar is now only displayed when the user is authenticated (i.e., logged in), making it unnecessary to show the login and register buttons.
    
    The logout button removes the access token cookie, sets the isAuth value from the authReducer to false using the 'logout' reducer, and redirects the user to the '/login' route.
    
    Updates styles for the app bar by adding a box shadow and enhances the 'Projects Manager' typography with better font styling.

--- frontend/src/common/NavBar.tsx
+++ frontend/src/common/NavBar.tsx
@@ -4,19 +4,39 @@ import {
   Button,
   Container,
   Grid,
-  Stack,
   Toolbar,
   Typography,
 } from '@mui/material';
 import React from 'react';
 import { themePalette } from '../config/theme.config';
 import { useNavigate } from 'react-router-dom';
+import { useAppDispatch } from '../redux/hooks';
+import { logout } from '../redux/slices/auth.slice';
+import { useCookies } from 'react-cookie';
+import COOKIE_NAMES from '../constants/cookie';
 
 export const NavBar: React.FC<{}> = () => {
+  const [cookies, setCookie, removeCookie] = useCookies([
+    COOKIE_NAMES.ACCESS_TOKEN,
+  ]);
   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
+  const handleLogout = () => {
+    removeCookie(COOKIE_NAMES.ACCESS_TOKEN, cookies.accessToken);
+    dispatch(logout());
+    navigate('/login');
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar sx={{ backgroundColor: themePalette.BG_2 }} position="fixed">
+      <AppBar
+        sx={{
+          backgroundColor: themePalette.BG_2,
+          boxShadow: '0px 2px 2px rgba(0, 0, 0, 0.2)',
+        }}
+        position="sticky"
+      >
         <Toolbar>
           <Container maxWidth="xl">
             <Grid
@@ -26,18 +46,18 @@ export const NavBar: React.FC<{}> = () => {
               alignItems={'center'}
             >
               <Grid item>
-                <Typography>Projects Manager</Typography>
+                <Typography
+                  sx={{
+                    fontWeight: 'bold',
+                    fontSize: 'x-large',
+                    letterSpacing: '-0.08rem',
+                    color: themePalette.PRIMARY,
+                  }}
+                >
+                  Projects Manager
+                </Typography>
               </Grid>
-              <Grid item>
-                <Stack spacing={2} direction={'row'}>
-                  <Button onClick={() => navigate('login')} variant="contained">
-                    Login
-                  </Button>
-                  <Button href="signup" variant="outlined">
-                    Register
-                  </Button>
-                </Stack>
-              </Grid>
+              <Button onClick={handleLogout}>Logout</Button>
             </Grid>
           </Container>
         </Toolbar>
--- frontend/src/common/RouterLayout.tsx
+++ frontend/src/common/RouterLayout.tsx
@@ -3,12 +3,30 @@ import { NavBar } from './NavBar';
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAppSelector } from '../redux/hooks';
 import { useNotification } from '../context/notification.context';
+import { useCookies } from 'react-cookie';
+import COOKIE_NAMES from '../constants/cookie';
 
 export const RouterLayout: React.FC<{}> = () => {
-  const { isAuth, error } = useAppSelector((state) => state.authReducer);
+  const [cookies, setCookie, removeCookie] = useCookies([
+    COOKIE_NAMES.ACCESS_TOKEN,
+  ]);
+  const { isAuth, error, accessToken, isExpired } = useAppSelector(
+    (state) => state.authReducer,
+  );
   const { getError } = useNotification();
 
-  if (error) getError(JSON.stringify(error));
+  React.useEffect(() => {
+    if (accessToken) setCookie(COOKIE_NAMES.ACCESS_TOKEN, accessToken);
+  }, [accessToken, setCookie]);
+
+  React.useEffect(() => {
+    if (isExpired) removeCookie(COOKIE_NAMES.ACCESS_TOKEN, cookies.accessToken);
+  }, [isExpired, removeCookie, cookies.accessToken]);
+
+  React.useEffect(() => {
+    if (error) getError(JSON.stringify(error));
+  }, [error, getError]);
+
   return isAuth ? (
     <>
       <NavBar />
