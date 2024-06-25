2575534a63b233a5ca5627f7152f2234504a784d
alvareztomas1 <47418131+alvareztomas1@users.noreply.github.com>
2024-06-25 15:15:55 -0300

    Front: Updates users.getById method
    Updates the method to receive the access token as a parameter and send it inside the header to the backend, so that it is analyzed by the backend guards and check if the user is authorized to execute the corresponding operation.

--- frontend/src/api/users.api.ts
+++ frontend/src/api/users.api.ts
@@ -46,10 +46,23 @@ export const users = {
       throw error;
     }
   },
-  getById: async (id: string) => {
+  getById: async (id: string, accessToken: string) => {
     try {
-      const response = await fetch(`${BASE_URL}${endpoint}/find/${id}`);
-      return await response.json();
+      const response = await fetch(`${BASE_URL}${endpoint}/find/${id}`, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          access_token: accessToken,
+          'Content-Type': 'application/json',
+        },
+      });
+      const responseJSON = await response.json();
+
+      if (!response.ok) {
+        throw new Error((responseJSON as Error).message);
+      }
+
+      return responseJSON;
     } catch (error) {
       throw error;
     }
