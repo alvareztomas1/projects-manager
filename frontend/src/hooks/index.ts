6a98a0e519136228c41cd88ebca90f49aa81fe27
alvareztomas1 <47418131+alvareztomas1@users.noreply.github.com>
2024-07-01 19:50:29 -0300

    Front: Updates custom hooks root folder with new hooks

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -2,5 +2,15 @@ import useNavBar from './navbar/useNavBar';
 import useLogin from './login/useLogin';
 import useSignup from './signup/useSignup';
 import useHomePage from './home/useHomePage';
-import useCreateProject from './home/useCreateProject';
-export { useNavBar, useLogin, useSignup, useHomePage, useCreateProject };
+import useSaveProject from './home/useSaveProject';
+import useDeleteProject from './home/useDeleteProject';
+import useProjectsList from './home/useProjectsList';
+export {
+  useNavBar,
+  useLogin,
+  useSignup,
+  useHomePage,
+  useSaveProject,
+  useDeleteProject,
+  useProjectsList,
+};
